eccd775c57c3f41dcddcac8e733578a42cbe4925
mike83 <mike.gomes.camara@ut.ee>
2021-08-30 22:35:56 +0300

    Upload project to GitHub

---
+++ index.jsx
@@ -0,0 +1,10 @@
+import { animals } from './animals';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const title = "";
+
+const animalFacts = <h1> {title == "" ? "Click an animal for a fun fact" : title} </h1>
+
+
+ReactDOM.render(animalFacts, document.getElementById('root'));
